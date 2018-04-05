482426e58e8642083e789ccd7b04387123bbfc49
Joel <joel.balmer@gmail.com>
2018-04-05 21:04:36 +0100

    added react-slider in

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ class App extends Component {
       <div className="App">
         <h1>Enter your sleep</h1>
         <h3>Time in bed</h3>
-        <input className="bed-time" type="range" min="0" max="5" />
+        <input className="bed-time" type="range" min="0" max="5" value=/>
         <h3>Time asleep</h3>
         <input className="bed-time" type="range" min="0" max="5" />
+        <h3>Double slider</h3>
+        <ReactSlider defaultValue={[0, 100]} withBars />
       </div>
     );
   }
