5daad2c0935bc618ed9bd5834ef701ddcfc27c63
Joel <joel.balmer@gmail.com>
2018-04-05 20:48:23 +0100

    Added sliders and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <input type="range" min="0" max="100" />
+        <h1>Enter your sleep</h1>
+        <h3>Time in bed</h3>
+        <input className="bed-time" type="range" min="0" max="5" />
+        <h3>Time asleep</h3>
+        <input className="bed-time" type="range" min="0" max="5" />
       </div>
     );
   }
