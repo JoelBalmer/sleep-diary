02b75101378d7845f8d51585abfba9ad9e63d836
Joel <joel.balmer@gmail.com>
2018-04-06 19:43:31 +0100

    Got passing values passing between elements, refactored to nicely generate code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from "react";
 import "./App.css";
+require("jquery-ui");
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      times: [2, 7, 13, 18]
+    };
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange(event) {
+    const newTimeIndex = event.currentTarget.getAttribute("event-order");
+    let newTimes = this.state.times.map(item => item);
+    newTimes[newTimeIndex] = event.target.value;
+    this.setState({
+      times: newTimes
+    });
+  }
+
   render() {
+    const sliderTexts = [
+      "got into bed",
+      "fell asleep",
+      "woke up",
+      "got out of bed"
+    ];
+
     return (
       <div className="App">
         <h1>Enter your sleep</h1>
-        <h3>Went to bed at</h3>
-        <input
-          className="bed-time"
-          type="range"
-          min="0"
-          max="10"
-          defaultValue="0"
-          list="times"
-          step="1"
-        />
+
+        {this.state.times.map((item, index) => {
+          return (
+            <div>
+              <h3>{"Enter the time you " + sliderTexts[index]}</h3>
+              <p className={"timeLabel timeLabel_" + index}>
+                {this.state.times[index]}
+              </p>
+              <input
+                className={"slider slider_" + index}
+                type="range"
+                min="0"
+                max="20"
+                step="1"
+                defaultValue={this.state.times[index]}
+                onChange={this.handleOnChange}
+                event-order={index}
+              />
+            </div>
+          );
+        })}
       </div>
     );
   }
