8fd6a6b00a370f83e0f55cba0238da643cbcdbfe
Joel <joel.balmer@gmail.com>
2018-04-09 12:21:39 +0100

    got bootstrap working nicely with columns, and displaying time in bed in overview section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import DateUtils from "./utils/time.js";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 //add date picker
@@ -36,17 +37,17 @@ class App extends Component {
     this.setState({
       times: newTimes
     });
-
-    console.log(value);
   }
 
   handleWakeChange(event) {
+    //convert slider value to minutes
     this.setState({
       wakeTime: event.target.value * 5
     });
   }
 
   render() {
+    //create array for less repeated code
     const sliderTexts = [
       "got into bed",
       "fell asleep",
@@ -57,50 +58,64 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>
-          {"Enter your sleep for " +
-            this.state.date.getDate() +
-            "/" +
-            this.state.date.getMonth() +
-            "/" +
-            this.state.date.getFullYear()}
-        </h1>
+        <div className="row">
+          <div className="diary-entry col-lg-9 col-md-9 col-sm-9 col-xs-9">
+            <h1>
+              {"Enter your sleep for " +
+                this.state.date.getDate() +
+                "/" +
+                this.state.date.getMonth() +
+                "/" +
+                this.state.date.getFullYear()}
+            </h1>
 
-        {this.state.times.map((item, index) => {
-          return (
-            <div>
-              <h3>{"Enter the time you " + sliderTexts[index]}</h3>
-              <p className={"timeLabel timeLabel_" + index}>
-                {DateUtils.getNewHours(
-                  this.state.date,
-                  this.state.times[index]
-                )}
-              </p>
-              <input
-                list="times"
-                className={"slider slider_" + index}
-                type="range"
-                min="0"
-                max="288"
-                defaultValue={this.state.times[index]}
-                onChange={this.handleOnChange}
-                event-order={index}
-              />
-            </div>
-          );
-        })}
+            {this.state.times.map((item, index) => {
+              return (
+                <div>
+                  <h3>{"Enter the time you " + sliderTexts[index]}</h3>
+                  <p className={"timeLabel timeLabel_" + index}>
+                    {DateUtils.getNewHours(
+                      this.state.date,
+                      this.state.times[index]
+                    )}
+                  </p>
+                  <input
+                    className={"slider slider_" + index}
+                    type="range"
+                    min="0"
+                    max="288"
+                    defaultValue={this.state.times[index]}
+                    onChange={this.handleOnChange}
+                    event-order={index}
+                  />
+                </div>
+              );
+            })}
 
-        <h3>{"Enter the time you " + sliderTexts[4]}</h3>
-        <p className="wake-time">
-          {DateUtils.formatWakeTime(this.state.wakeTime)}
-        </p>
-        <input
-          type="range"
-          min="0"
-          max="144"
-          defaultValue="0"
-          onChange={this.handleWakeChange}
-        />
+            <h3>{"Enter the time you " + sliderTexts[4]}</h3>
+            <p className="wake-time">
+              {DateUtils.formatWakeTime(this.state.wakeTime)}
+            </p>
+            <input
+              type="range"
+              min="0"
+              max="144"
+              defaultValue="0"
+              onChange={this.handleWakeChange}
+            />
+          </div>
+          <div className="overview col-lg-3 col-md-3 col-sm-3 col-xs-3">
+            <h1>Overview</h1>
+            <h3>Time in bed</h3>
+            <p>
+              {DateUtils.subtractDates(
+                this.state.date,
+                this.state.times[0],
+                this.state.times[3]
+              )}
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
--- src/utils/time.js
+++ src/utils/time.js
@@ -4,13 +4,15 @@ const formatZero = value => {
 	if (value < 10) {
 		return "0" + value;
 	}
-
 	return value;
 };
 
+const minsToMils = mins => {
+	return mins * 5 * 60 * 1000;
+};
+
 exports.getNewHours = (date, timeToAdd) => {
-	let milsToAdd = timeToAdd * 5 * 60 * 1000;
-	let totalMils = Number(date.getTime()) + Number(milsToAdd);
+	let totalMils = Number(date.getTime()) + Number(minsToMils(timeToAdd));
 	let newDate = new Date(totalMils);
 	return moment(newDate).format("h:mm a");
 };
@@ -19,4 +21,11 @@ exports.formatWakeTime = mins => {
 	let hours = Math.floor(mins / 60);
 	let minutes = mins % 60;
 	return formatZero(hours) + "h " + formatZero(minutes) + "m";
+};
+
+exports.subtractDates = (date, mins1, mins2) => {
+	let startDate = moment(date).add(minsToMils(mins1));
+	let endDate = moment(date).add(minsToMils(mins2));
+	let difference = moment(endDate).diff(startDate, "minutes");
+	return this.formatWakeTime(difference);
 };
