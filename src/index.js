935e836425054430d0860428591c4a4b55de95b8
Joel <joel.balmer@gmail.com>
2018-07-16 20:57:38 +0100

    Prepping for express

--- src/index.js
+++ src/index.js
@@ -4,9 +4,5 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-const keys = require("./config/keys");
-let passport = require("passport"),
-  FacebookStrategy = require("passport-facebook").Strategy;
-
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
