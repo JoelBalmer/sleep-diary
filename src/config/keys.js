3ee86fd39331989a16154de7095aa7f6a35083d3
Joel <joel.balmer@gmail.com>
2018-07-16 13:33:03 +0100

    Updated npm packages, added facbeook keys, installed passport packages

---
+++ src/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  FACEBOOK_APP_ID: "267207070751883",
+  FACEBOOK_APP_SECRET: "a9274e8149c7bc66ba8669caf607b1a9"
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const keys = require("./config/keys");
+let passport = require("passport"),
+  FacebookStrategy = require("passport-facebook").Strategy;
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
