ff667ae36ca213bb91175ccfb7a013e3e66f1dbf
Joel <joel.balmer@gmail.com>
2018-07-21 10:02:46 +0100

    Updated a post successfully

--- backend/routes/entries.js
+++ backend/routes/entries.js
@@ -19,22 +19,27 @@ router.post("/test", function(req, res, next) {
 // @route   POST api/users/test
 // @desc    Tests users route
 router.post("/entry", (req, res) => {
+  const bodyObject = {
+    uid: req.body.uid,
+    date: req.body.date,
+    start_bed: req.body.start_bed,
+    start_sleep: req.body.start_sleep,
+    end_sleep: req.body.end_sleep,
+    end_bed: req.body.end_bed,
+    awake: req.body.awake,
+    description: req.body.description,
+    rating: req.body.rating
+  };
+
   Entry.findOne({ date: req.body.date }).then(entry => {
     if (entry) {
-      console.log("update entry");
+      Object.assign(entry, bodyObject);
+      entry
+        .save()
+        .then(savedEntry => res.json(savedEntry))
+        .catch(err => console.log(`Here's the error: ${err}`));
     } else {
-      const newEntry = new Entry({
-        uid: req.body.uid,
-        date: req.body.date,
-        start_bed: req.body.start_bed,
-        start_sleep: req.body.start_sleep,
-        end_sleep: req.body.end_sleep,
-        end_bed: req.body.end_bed,
-        awake: req.body.awake,
-        description: req.body.description,
-        rating: req.body.rating
-      });
-
+      const newEntry = new Entry(bodyObject);
       newEntry
         .save()
         .then(entry => res.json(entry))
