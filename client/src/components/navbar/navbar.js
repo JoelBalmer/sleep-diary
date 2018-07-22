f5a3cf0d72ddc0925cd194ddc4cd3a06be6c762c
Joel <joel.balmer@gmail.com>
2018-07-22 10:50:37 +0100

    Sorted out menu

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -8,16 +8,18 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <div className="navbar nav-bar d-flex p-2">
-        <div className="logo p-2 bd-highlight">
-          <a className="logo p-2 bd-highlight" href="http://localhost:3000/">
+      <div className="navbar nav-bar">
+        <div className="logo nav navbar-nav pull-sm-left">
+          <a className="logo-link" href="http://localhost:3000/">
             <img src="./img/logo.png" />
           </a>
         </div>
-        <h1 className="title justify-content-center">{this.props.title}</h1>
-        <a className="login p-2 bd-highlight" href="http://localhost:3000/">
-          <h2 className="">Login</h2>
-        </a>
+        <h1 className="title nav navbar-nav mx-auto">{this.props.title}</h1>
+        <div className="login nav navbar-nav pull-sm-right">
+          <a className="login-link" href="http://localhost:3000/">
+            <h2 className="">Hello, Constantine!</h2>
+          </a>
+        </div>
       </div>
     );
   }
