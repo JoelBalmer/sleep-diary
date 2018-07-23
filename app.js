90abd21136bfa15386fca1069489655fd31fe271
Joel <joel.balmer@gmail.com>
2018-07-23 12:41:37 +0100

    removed render call to try and solve issue

--- app.js
+++ app.js
@@ -92,8 +92,10 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: err
+  });
 });
 
 // DB Config
