00666ae1ff41b39258091b9b5f5e9d9794f9c533
Joel <joel.balmer@gmail.com>
2018-07-25 23:24:05 +0100

    uncommented previous error logging

--- app.js
+++ app.js
@@ -103,23 +103,23 @@ const errorHandler = (err, req, res, next) => {
 
 app.use(errorHandler);
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500).json({
-//     message: err.message,
-//     error: err
-//   });
-// });
+  // render the error page
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: err
+  });
+});
 
 // DB Config
 const db = process.env.MONGODB_URI || require("./config/keys").mongoURI;
