6db29e4beaf07ce3bbb7b9fa2af0a8f7c1672947
Joel <joel.balmer@gmail.com>
2018-07-30 16:02:39 +0100

    Check profile to see if logged in, then get entries for taht users id

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     this.state = {
       view: "Sleep diary",
       entries: [],
-      nameText: "login"
+      nameText: "Login"
     };
 
     this.handleCalendarClick = this.handleCalendarClick.bind(this);
@@ -74,19 +74,6 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("/entries")
-      .then(res => res.json())
-      .then(entries => {
-        let newEntries = [];
-        entries.forEach(entry => {
-          newEntries.push(new Date(entry.date));
-        });
-        this.setState({
-          view: "Sleep diary",
-          entries: newEntries
-        });
-      });
-
     fetch("/profile")
       .then(res => res.json())
       .then(profile => {
@@ -94,6 +81,21 @@ class App extends Component {
           nameText: profile.nameText,
           uid: profile.userId
         });
+
+        if (Number(profile.userId)) {
+          fetch("/entries/" + profile.userId)
+            .then(res => res.json())
+            .then(entries => {
+              let newEntries = [];
+              entries.forEach(entry => {
+                newEntries.push(new Date(entry.date));
+              });
+              this.setState({
+                view: "Sleep diary",
+                entries: newEntries
+              });
+            });
+        }
       });
   }
 
