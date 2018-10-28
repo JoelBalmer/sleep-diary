490f0456b816879945cdd69c948de4835378b916
Joel <joel.balmer@gmail.com>
2018-10-28 18:43:04 +0000

    Successfully fetching entries

--- client/src/components/overview/overview.js
+++ client/src/components/overview/overview.js
@@ -6,11 +6,26 @@ class Overview extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    fetch("http://localhost:3001/entries")
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("something went wrong");
+        }
+      })
+      .then(entries => {
+        console.log(entries);
+      });
+  }
+
   render() {
     return (
       <div className="overview">
         <h1 className="overview-title">This is your overview!</h1>
         <div className="graph" />
+        />
       </div>
     );
   }
