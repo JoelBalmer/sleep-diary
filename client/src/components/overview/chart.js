30852da106db5eef998e9293b7893bd5b1f74cef
Joel <joel.balmer@gmail.com>
2018-11-02 10:59:34 +0000

    see previous commit

---
+++ client/src/components/overview/chart.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  VerticalGridLines,
+  HorizontalGridLines,
+  LineMarkSeries
+} from "react-vis";
+import "./chart-style.css";
+
+const Chart = props => {
+  const dataArray = props.data.map(currentDataPoint => {
+    const date = new Date(currentDataPoint.date);
+    var dateString = "" + date.getDay() + "/" + (date.getMonth() + 1);
+
+    return {
+      x: dateString,
+      y: currentDataPoint.rating
+    };
+  });
+
+  return (
+    <XYPlot width={1000} height={500} xType="ordinal">
+      <VerticalGridLines />
+      <HorizontalGridLines />
+      <XAxis title="Date" />
+      <YAxis title="Rating" />
+      <LineMarkSeries
+        className="linemark-series-example"
+        style={{
+          strokeWidth: "3px"
+        }}
+        lineStyle={{ stroke: "red" }}
+        markStyle={{ stroke: "blue" }}
+        data={dataArray}
+      />
+    </XYPlot>
+  );
+};
+
+export default Chart;
