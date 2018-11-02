db339797a6d3ad4b2e889420ac20c4965d158412
Joel <joel.balmer@gmail.com>
2018-11-02 10:58:49 +0000

    Added data vis working and pulling data

--- client/src/components/overview/overview.js
+++ client/src/components/overview/overview.js
@@ -1,31 +1,51 @@
-import "./overview.css";
 import React from "react";
+import Chart from "./chart.js";
+import "./overview.css";
 
 class Overview extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      results: []
+    };
   }
 
   componentDidMount() {
     fetch("http://localhost:3001/entries")
       .then(response => {
         if (response.ok) {
-          return response.json();
+          let data = response.json();
+          return data;
         } else {
-          throw new Error("something went wrong");
+          throw new Error("Something went wrong");
         }
       })
-      .then(entries => {
-        console.log(entries);
+      .then(response => {
+        let resultsArray = [];
+        for (let i = 0; i < response.length; i++) {
+          let currentEntry = response[i];
+          let newEntry = {
+            date: currentEntry.date,
+            rating: currentEntry.rating
+          };
+          resultsArray.push(newEntry);
+        }
+
+        this.setState({
+          results: resultsArray
+        });
       });
   }
 
   render() {
+    const { results } = this.state;
+
     return (
       <div className="overview">
         <h1 className="overview-title">This is your overview!</h1>
-        <div className="graph" />
-        />
+        <div className="graph">
+          <Chart data={this.state.results} />
+        </div>
       </div>
     );
   }
